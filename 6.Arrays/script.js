6deb4940b36e56f1af21750fc00847c586032dd0
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-01-25 23:45:20 +0530

    Arrays Intro (Ops)

---
+++ 6.Arrays/script.js
@@ -0,0 +1,30 @@
+// --------------- Creation of Array ------
+
+// const arr = [1, 2, 3, 4];
+// const arr2 = new Array();
+
+// // java ->
+// // int[] arr = new int[4]
+
+// console.log(arr, arr2);
+
+//  ---------------------- Equating Arrays ----------------
+
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [1, 2, 3, 4];
+
+// console.log(arr1 === arr2); //false
+
+// const arr3 = arr1;
+
+// console.log(arr2 === arr3); // false
+// console.log(arr1 === arr3); // true
+
+//  --------------------- Operations on Array ----------------
+
+// 1. push
+
+const arr = [12, 3, 4];
+
+console.log(arr.push(100)); // 4
+console.log(arr);
