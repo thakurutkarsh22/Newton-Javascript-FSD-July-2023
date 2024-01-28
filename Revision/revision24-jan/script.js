10a605d34e86c9e55910578060481f279ee1e135
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-01-28 21:38:16 +0530

    doubts + revision

--- Revision/revision24-jan/script.js
+++ Revision/revision24-jan/script.js
@@ -28,24 +28,72 @@
 
 // console.log(criticalFunctoin());
 
-function criticalFunctoin() {
-  try {
-    const a = 11;
-    const b = 21;
+// function criticalFunctoin() {
+//   try {
+//     const a = 11;
+//     const b = 21;
 
-    const c = a + b;
-    // logic a + b;
+//     const c = a + b;
+//     // logic a + b;
 
-    // logging "a+b" // google services console.log() google.log()
-    throw Error("Google logger Number required but you send String"); // have i done anything about this error ?
+//     // logging "a+b" // google services console.log() google.log()
+//     throw Error("Google logger Number required but you send String"); // have i done anything about this error ?
 
-    return c;
-  } catch (error) {
-    // Handle the error gracefully..  Beautiful Page ....
-    console.log("----------");
-    alert(error.message);
-    console.log(error);
-  }
+//     return c;
+//   } catch (error) {
+//     // Handle the error gracefully..  Beautiful Page ....
+//     console.log("----------");
+//     alert(error.message);
+//     console.log(error);
+//   }
+// }
+
+// console.log(criticalFunctoin());
+
+// let b = 9;
+// const d = 12
+// var x = 12;
+
+// a = 12;
+
+// console.log(a, x);
+
+var a = 99;
+
+function abc() {
+  console.log(a);
 }
 
-console.log(criticalFunctoin());
+abc();
+
+// Question: What is HOF and callback function
+
+// a and b are Parameters
+// hof 1. accept a Function in parameter
+// 2. a function that returns a function
+// function abc(a , b) {
+//   return () => {}
+// }
+
+// function sum(a,b) {
+
+// }
+
+// const sum1 = function (a,b) {
+
+// }
+
+// const sum2 = (a, b) => {
+
+//   function abc() {
+
+//   }
+//   return () => {};
+// }
+
+// // sum1 -> is a function
+// // sum1() // value
+
+// abc( () => {}  ,3) // 1,3 are Arguments
+
+// callback function -> its a function only but in Arguments.
