ae785ae5ba76de3058873baa9fdb0900e7a0ff92
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-02-04 16:47:01 +0530

    More on DOM (insertion, Fragments)

--- ClassLecture/9.DOM/script.js
+++ ClassLecture/9.DOM/script.js
@@ -1,11 +1,28 @@
 // HTML -> DOCUMENT (both the things are same);
 
+//  ------------------ DOM TREE -----------------
+
+// const containerElement = document.getElementById("container");
+// containerElement.append(newHeading1Element);
+
+// DOM tree is created By Browser Itself.
+// Tree is choosed bec it is a good representation of Relationship B/w Nodes. (Element in ram)
+
+//NOTE: TO VISUALIZE DOM TREE: https://bioub.github.io/dom-visualizer/
+
+/*
+  Types of Nodes
+  1. Element Node (h1, div, HTML, body, P)
+  2. Text Node (Real text and Space)  
+  3. Comment Node (Html Comments)
+*/
+
 //  ------------------- SELECTORS (get the element) -------------------
 
 // 1. getElementById
 
 // const headingElement = document.getElementById("heading");
-// // console.log(headingElement); // "English readable Element"
+// console.log(headingElement, "heading element"); // "English readable Element"
 // console.dir(headingElement); // "REAL OBject";
 
 // 2. getElementsByTagName.
@@ -71,15 +88,132 @@
 
 */
 
-const newHeading1Element = document.createElement("h1");
-console.log(newHeading1Element);
+// const newHeading1Element = document.createElement("h1");
+// console.log(newHeading1Element);
 
-newHeading1Element.textContent = "xyz";
-newHeading1Element.style.backgroundColor = "aqua";
+// newHeading1Element.textContent = "xyz";
+// newHeading1Element.style.backgroundColor = "aqua";
 
-const bodyElement = document.body;
-bodyElement.prepend(newHeading1Element);
-console.log(bodyElement);
+// const bodyElement = document.body;
+// bodyElement.prepend(newHeading1Element);
+// console.log(bodyElement);
 
-// const containerElement = document.getElementById("container");
-// containerElement.append(newHeading1Element);
+// --------------------------- ------- Advance Editing Element ----------------
+
+// ------------ Insertion ---------
+
+// 1. append. -> add item in the END of the list/Element
+
+// const namesListElement = document.getElementById("names"); // ul
+// console.dir(namesListElement);
+
+// const studentList = ["Amith", "Saurav", "yash", "hemant"]; // from database I got it. Do not worry about how I got it.
+
+// for (let index = 0; index < studentList.length; index++) {
+//   const studentName = studentList[index];
+
+//   // Create an LI Element
+//   const listItemElement = document.createElement("li"); // <li></li>
+//   listItemElement.textContent = studentName; // <li>StudentName</li> // this thing is NOT ATTACHED TO DOM TREE
+
+//   //   Now we have to insert the element
+//   // the question is Where to Insert
+//   namesListElement.append(listItemElement); // Now this is attached...
+// }
+
+// 2. Prepend -> add item in the start of the list/element
+
+// const namesListElement = document.getElementById("names"); // ul
+// console.dir(namesListElement);
+
+// const studentList = ["Amith", "Saurav", "yash", "hemant"]; // from database I got it. Do not worry about how I got it.
+
+// for (let index = 0; index < studentList.length; index++) {
+//   const studentName = studentList[index];
+
+//   // Create an LI Element
+//   const listItemElement = document.createElement("li"); // <li></li>
+//   listItemElement.textContent = studentName; // <li>StudentName</li> // this thing is NOT ATTACHED TO DOM TREE
+
+//   //   Now we have to insert the element
+//   // the question is Where to Insert
+//   namesListElement.prepend(listItemElement); // Now this is attached...
+// }
+
+// 3. before -> insert element before the selected Element
+// const namesListElement = document.getElementById("names"); // ul
+// const heading1Element = document.createElement("h2");
+// heading1Element.textContent = "Here's  the list of student";
+
+// namesListElement.before(heading1Element);
+
+// 4. after -> insert element after the selected Element
+// const footerElement = document.createElement("h3");
+// footerElement.textContent = "End of the list";
+
+// namesListElement.after(footerElement);
+
+// 5. replaceWith -> replaces node with the given nodes or strings.
+
+// --------- Another Way to Insert Element insertAdjacentHTML --------------
+// 1. "beforebegin" – insert html immediately before elem,
+// 2. "afterbegin" – insert html into elem, at the beginning,
+// 3. "beforeend" – insert html into elem, at the end,
+// 4. "afterend" – insert html immediately after elem.
+
+// NOTE: Read the above things from: https://javascript.info/modifying-document.
+
+// --------  how to select body element -----------
+
+// const body = document.body;
+// const body = document.getElementsByTagName("body")[0];
+
+// --------------------- ------- Fragments ---------
+/*
+    Question is are we inserting in a better way ? 
+    Answer NO. 
+
+
+    const namesListElement = document.getElementById("names"); // ul
+    console.dir(namesListElement);
+
+    const studentList = ["Amith", "Saurav", "yash", "hemant"]; // from database I got it. Do not worry about how I got it.
+
+    for (let index = 0; index < studentList.length; index++) {
+    const studentName = studentList[index];
+
+    // Create an LI Element
+    const listItemElement = document.createElement("li"); // <li></li>
+    listItemElement.textContent = studentName; // <li>StudentName</li> // this thing is NOT ATTACHED TO DOM TREE
+
+    //   Now we have to insert the element
+    // the question is Where to Insert
+    namesListElement.append(listItemElement); // Now this is attached...
+    }
+
+
+    In this code WE WENT TO DOM and Request 4 TIMES to Insert all the values 
+*/
+
+// We will use Fragments ...
+
+// const fakeNodeOfNamesListElement = new DocumentFragment();
+
+// const namesListElement = document.getElementById("names");
+// console.dir(namesListElement);
+
+// const studentList = ["Amith", "Saurav", "yash", "hemant", "utkarsh"];
+
+// for (let index = 0; index < studentList.length; index++) {
+//   const studentName = studentList[index];
+
+//   // Create an LI Element
+//   const listItemElement = document.createElement("li");
+//   listItemElement.textContent = studentName;
+
+//   // Insted Of adding the Element inside the DOM (namesListElement) we should Add it someplace else.
+
+//   //   namesListElement.append(listItemElement);
+//   fakeNodeOfNamesListElement.append(listItemElement);
+// }
+// namesListElement.append(fakeNodeOfNamesListElement);
