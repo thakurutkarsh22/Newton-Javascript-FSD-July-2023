9bafc1eba685d144bc04b4d116406231bdbc9e19
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-02-08 23:51:38 +0530

    Types of Event

--- ClassLecture/11.Events/script.js
+++ ClassLecture/11.Events/script.js
@@ -22,7 +22,7 @@ console.log("a");
 // 2. We specify what EVENT we want to listen on that ELEMENT.
 // 3. Ofcourse the Callback function when that event arrives.
 
-// const button1Element = document.getElementById("button1");
+const button1Element = document.getElementById("button1");
 // console.log(button1Element);
 
 // function button1Callback() {
@@ -32,17 +32,66 @@ console.log("a");
 
 // button1Element.addEventListener("click", button1Callback);
 
+// const containerELement = document.getElementById("container");
+// console.log(containerELement);
+
+// containerELement.addEventListener("mouseout", () => {
+//   containerELement.style.background = "red";
+// });
+
+// containerELement.addEventListener("click", () => {
+//   containerELement.style.background = "purple";
+// });
+
+// containerELement.addEventListener("mouseenter", () => {
+//   containerELement.style.background = "yellow";
+// });
+
+// const listOFElements = document.getElementsByClassName("color");
+// const colorElement = listOFElements[0];
+
+// console.log(colorElement);
+
+// colorElement.addEventListener("click", () => {
+//   colorElement.style.background = "yellow";
+// });
+
+//  ------------------ Mouse Events ----------------------
+// 1. Click
+
+// function button1Callback() {
+//   button1Element.style.background = "red";
+//   console.log("click buuton  1");
+// }
+
+// button1Element.addEventListener("click", button1Callback);
+
+// 2. Double Click
+
+// const containerELement = document.getElementById("container");
+// containerELement.addEventListener("dblclick", () => {
+//   console.log("double click 1!!!");
+// });
+
+// 3. mouseOver: CARES ABOUT CHILD (read MDN DOCS)
+
+// const containerELement = document.getElementById("container");
+// containerELement.addEventListener("mouseover", () => {
+//   console.log("mouseover !!!");
+// });
+
+// 4. mouseEnter: Mouse Enter DO NOT CARE ABOUT CHILD  (read MDN DOCS)
+
+// const containerELement = document.getElementById("container");
+// containerELement.addEventListener("mouseenter", () => {
+//   console.log("mouseenter !!!");
+// });
+
+// ------------------------- WHAT EXACTLY ARE EVENTS -----------------
+
+// --------------- KEYBOARD EVENTS -----------------------
+
 const containerELement = document.getElementById("container");
-console.log(containerELement);
-
-containerELement.addEventListener("mouseout", () => {
-  containerELement.style.background = "red";
-});
-
-containerELement.addEventListener("click", () => {
-  containerELement.style.background = "purple";
-});
-
-containerELement.addEventListener("mouseenter", () => {
-  containerELement.style.background = "yellow";
+containerELement.addEventListener("key", () => {
+  console.log("mouseenter !!!");
 });
