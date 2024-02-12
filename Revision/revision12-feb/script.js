1ccaeaf31cc995758de3eb37810b1ace4a5bcd7c
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-02-12 23:37:49 +0530

    Revision : 12th - feb 2024

---
+++ Revision/revision12-feb/script.js
@@ -0,0 +1,67 @@
+/*
+    Callback Function
+*/
+
+// function greeting(func) {
+//   // own work
+
+//   const a = 12;
+//   console.log("my log is ", a);
+
+//   //   later on it is calling the func..
+//   func();
+// }
+
+// function hindi() {
+//   console.log("Namaste!!");
+// }
+
+// function english() {
+//   console.log("Hello!");
+// }
+
+// function french() {
+//   console.log("borjour!!!");
+// }
+
+// // greet Hindi people
+
+// greeting(hindi);
+
+// hindi is a callback function
+
+/*
+
+    Arrays.map 
+
+*/
+
+// const arr = [1, 2, 3, 4, 5];
+
+// double each element inside this array..
+// const newArray = [];
+
+// for (let index = 0; index < arr.length; index++) {
+//   const element = arr[index];
+//   newArray[index] = 2 * element;
+// }
+
+// const mappedArray = arr.map((item) => {
+//   return 2 * item;
+// });
+
+// console.log(mappedArray);
+// console.log(arr);
+
+// {} + 1
+
+// {} + {}
+
+// [] + []
+
+/*
+
+    
+
+
+*/
