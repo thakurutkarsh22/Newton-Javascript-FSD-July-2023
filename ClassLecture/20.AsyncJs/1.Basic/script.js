788f6b3dd79eb9d7345ac3d312edaff8412e992d
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-02-28 23:47:23 +0530

    setTimeout 10 output Questions

--- ClassLecture/20.AsyncJs/1.Basic/script.js
+++ ClassLecture/20.AsyncJs/1.Basic/script.js
@@ -62,3 +62,162 @@ console.log("m");
 */
 
 // NOTE : Video about event loop https://www.youtube.com/watch?v=8aGhZQkoFbQ
+
+// Question 1:
+
+/*console.log("a")
+
+try {
+  setTimeout(() => {
+    console.log("hello");
+  }, 2000);
+} catch (err) {
+  console.log("hi");
+
+  console.log(err);
+}
+
+console.log("c")
+
+*/
+
+// answer: a, c and hello after 2 seconds.
+
+// Question 2
+
+/*
+try {
+  setTimeout(() => {
+    throw new Error("hello");
+  }, 2000);
+
+  console.log("a");
+} catch (err) {
+  console.log(err);
+}
+*/
+
+// answer : a, hello Error in RED
+
+// Question 3:
+
+/*try {
+  console.log("a");
+  throw new Error("hello");
+} catch (err) {
+  console.log(err);
+}
+*/
+
+// answer : a, hello Error in NOT RED (basically handled) .
+
+// Question 4:
+
+let obj = {
+  a: 1000,
+  func: function () {
+    let b = 12;
+    setTimeout(function () {
+      console.log("hello", this.a);
+    }, 2000);
+  },
+};
+
+obj.func();
+
+// answer: hello undefined.
+
+// Question 5:
+
+let obj2 = {
+  a: 1000,
+  func: function () {
+    let b = 12;
+    setTimeout(() => {
+      console.log("hello", this.a);
+    }, 2000);
+  },
+};
+
+obj2.func();
+
+// answer: hello 1000
+
+// Question 6:
+
+let obj3 = {
+  a: 1000,
+  func: function () {
+    let b = 12;
+    setTimeout(() => {
+      console.log("hello", this.a, b);
+    }, 2000);
+  },
+};
+
+obj3.func();
+
+// answer: hello 1000 12
+
+// Question 7:
+
+let obj4 = {
+  a: 1000,
+  func: function () {
+    let b = 12;
+    setTimeout(function () {
+      console.log("hello", this.a, b);
+    }, 2000);
+  },
+};
+
+obj4.func();
+
+// answer hello undef 12\
+
+// ------------- ClearTimeout -----------------
+
+const id = setTimeout(() => {
+  console.log("after 15 seconds");
+}, 15000);
+
+clearTimeout(id);
+
+// with this Settimeout CallbackFunction will not be executed.
+
+// Question 8:
+
+console.log("A");
+
+setTimeout(() => {
+  console.log("B");
+}, 0);
+
+console.log("c");
+
+console.log("D");
+
+// answer: A, c, B
+
+// Question 9:
+
+var a = 12;
+
+setTimeout(() => {
+  console.log(a);
+  var a = 99;
+}, 0);
+console.log("a");
+
+// answer: a undefined
+
+// Question 10:
+
+var a = 12;
+
+setTimeout(() => {
+  console.log(a);
+}, 0);
+console.log("a");
+
+// answer: a 12
